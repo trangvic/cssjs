5c5108236b5dd4e86b9bf1a0023154e31ff6b31a
trangvic <trangluu27989@gmail.com>
2017-05-30 15:14:03 +0700

    Add files via upload

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'http://cuahangvespachinhhangtaihanoi.blogspot.com/';
+	var url_popup = 'https://www.facebook.com/Piaggio-384835935026233/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
